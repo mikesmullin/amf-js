62b3bf42f33b5d96390cf2fce337f19d55b6e8c5
Mike Smullin <mike@smullindesign.com>
2016-07-17 12:32:14 -0600

    several new types supported

--- src/amf.js
+++ src/amf.js
@@ -108,6 +108,7 @@ var AMF = (function(){var AMF = {
   Deserializer: function(arrayBuffer) {
     var buf = arrayBuffer;
     var pos = 0;
+    var _this = this;
 
     var readByte = function() {
       return new DataView(buf)
@@ -122,25 +123,31 @@ var AMF = (function(){var AMF = {
     };
 
     this.deserialize = function() {
-      switch (readByte()) {
+      var b = readByte();
+      switch (b) {
+        case AMF.AMF3_UNDEFINED:
+          return undefined;
+        case AMF.AMF3_NULL:
+          return null;
+        case AMF.AMF3_FALSE:
+          return false;
+        case AMF.AMF3_TRUE:
+          return true;
         case AMF.AMF3_INT:
           return readInt();
-          break;
-
+        case AMF.AMF3_DOUBLE:
+          return readDouble();
         case AMF.AMF3_STRING:
           return readString();
-          break;
-
+        case AMF.AMF3_OBJECT:
+          return readObject();
         default:
           throw new Error("Unrecognized type marker "+ AMF.hexDump(b) +". Cannot proceed with deserialization.");
-          break;
       }
     };
 
-
     var readInt = function() {
       var result = 0;
-
       var n = 0;
       var b = readByte();
       while ((b & 0x80) !== 0 && n < 3) {
@@ -159,8 +166,14 @@ var AMF = (function(){var AMF = {
           result |= 0xE0000000;
         }
       }
-
       return result;
+    };
+
+    var readDouble = function() {
+      var f = new DataView(buf)
+        .getFloat64(pos, AMF.BIG_ENDIAN);
+      pos += 8;
+      return f;
     };
 
     var stringReferences = [];
@@ -168,17 +181,75 @@ var AMF = (function(){var AMF = {
       var reference = readInt();
       if (0 === (reference & AMF.REFERENCE_BIT)) {
         reference >>= AMF.REFERENCE_BIT;
-        return stringReferences[reference];
+        s = stringReferences[reference];
+        return s;
       }
-
       var length = reference >> AMF.REFERENCE_BIT;
       var data = new Uint8Array(buf, pos, length);
-      var string = decodeURIComponent(AMF.hexDump(data).replace(/\s+/g, '').replace(/[0-9a-f]{2}/g, '%$&'));
-      stringReferences.push(string);
+      var string = decodeURIComponent(
+        AMF.hexDump(data).replace(/\s+/g, '')
+          .replace(/[0-9a-f]{2}/g, '%$&'));
+      if (length > 0) {
+        stringReferences.push(string);
+      }
+      pos += length;
       return string;
     };
 
     var objectReferences = [];
+    var clsAliases = {};
+    var merge = function(instance, data) {
+      try {
+        for (var key in data) {
+          var val = data[key];
+          instance[key] = val;
+        }
+      } catch(e) {
+        // some properties may not be public
+        throw new Error("Property '" + key + "' cannot be set on instance '" + (typeof instance) + "'");
+      }
+    };
+    var readObject = function() {
+      var reference = readInt();
+      if (0 === (reference & AMF.REFERENCE_BIT)) {
+        reference >>= AMF.REFERENCE_BIT;
+        return objectReferences[reference];
+      }
+
+      var clsAlias = readString();
+
+      // add a new reference at this stage - essential to handle self-referencing objects
+      var instance = {};
+      objectReferences.push(instance);
+
+      // collect all properties into hash
+      var data = {};
+      var property = readString();
+      while (property.length) {
+        data[property] = _this.deserialize();
+        property = readString();
+      }
+
+      // when clsAlias is given, initialization of an existing type is implied
+      if (clsAlias && clsAlias.length > 0) {
+        var classType = clsAliases[clsAlias];
+        if (!classType) {
+          throw new Error('Class ' + clsAlias + ' cannot be found. Consider registering a class alias.');
+        }
+        instance = new classType;
+        if ('importData' in instance &&
+          'function' == typeof instance.importData)
+        {
+          instance.importData(data);
+        } else {
+          merge(instance, data);
+        }
+      } else {
+        merge(instance, data);
+      }
+
+      return instance;
+    };
   }
 };
 
--- test/everything.js
+++ test/everything.js
@@ -16,5 +16,53 @@ describe('Array', () => {
       let d = new AMF.Deserializer(buf);
       assert.equal(d.deserialize(), "hi");
     });
+
+    it('can parse utf-8 string "⑥⑥⑥"', () => {
+      let buf = AMF.makeArrayBuffer([0x06, 0x13, 0xe2, 0x91, 0xa5, 0xe2, 0x91, 0xa5, 0xe2, 0x91, 0xa5]);
+      let d = new AMF.Deserializer(buf);
+      assert.equal(d.deserialize(), "⑥⑥⑥");
+    });
+
+    it('can parse dynamic object { him: 3 }', () => {
+      let buf = AMF.makeArrayBuffer([0x0a, 0x0b, 0x01, 0x07, 0x68, 0x69, 0x6d, 0x04, 0x03, 0x01]);
+      let d = new AMF.Deserializer(buf);
+      assert.deepEqual(d.deserialize(), { him: 3 });
+    });
+
+    it('can parse reference strings { hi: "hi" }', () => {
+      let buf = AMF.makeArrayBuffer([0x0a, 0x0b, 0x01, 0x05, 0x68, 0x69, 0x06, 0x00, 0x01]);
+      let d = new AMF.Deserializer(buf);
+      assert.deepEqual(d.deserialize(), { hi: "hi" });
+    });
+
+    it('can parse undefined', () => {
+      let buf = AMF.makeArrayBuffer([0x00]);
+      let d = new AMF.Deserializer(buf);
+      assert.equal(d.deserialize(), undefined);
+    });
+
+    it('can parse null', () => {
+      let buf = AMF.makeArrayBuffer([0x01]);
+      let d = new AMF.Deserializer(buf);
+      assert.equal(d.deserialize(), null);
+    });
+
+    it('can parse false', () => {
+      let buf = AMF.makeArrayBuffer([0x02]);
+      let d = new AMF.Deserializer(buf);
+      assert.equal(d.deserialize(), false);
+    });
+
+    it('can parse true', () => {
+      let buf = AMF.makeArrayBuffer([0x03]);
+      let d = new AMF.Deserializer(buf);
+      assert.equal(d.deserialize(), true);
+    });
+
+    it('can parse double 15.24', () => {
+      let buf = AMF.makeArrayBuffer([0x05, 0x40, 0x2e, 0x7a, 0xe1, 0x47, 0xae, 0x14, 0x7b]);
+      let d = new AMF.Deserializer(buf);
+      assert.equal(d.deserialize(), 15.24);
+    });
   });
 });
